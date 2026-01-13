1ddcf372a567345de88681bb35c759abaa21bce0
Gerald <gerald974@revature.net>
2026-01-13 13:30:57 -0600

    reindented health

--- src/main/java/com/revature/health/HealthController.java
+++ src/main/java/com/revature/health/HealthController.java
@@ -5,19 +5,19 @@ import io.javalin.http.HttpStatus;
 
 public class HealthController {
 
-		public void ping(Context ctx) {
-				String reply = "pong!";
-				ctx.result(reply);
-		}
+	public void ping(Context ctx) {
+		String reply = "pong!";
+		ctx.result(reply);
+	}
 
-		public void headerTest(Context ctx) {
-				String headerString = ctx.header("header-test");
-				ctx.header("header-test-response", String.format("Response: %s", headerString));
-				if (headerString != null) {
-						ctx.result(String.format("You sent %s in the header", headerString)).status(HttpStatus.OK);
-				}
-				else {
-						ctx.result("You didn't specify header-test").status(HttpStatus.BAD_REQUEST);
-				}
+	public void headerTest(Context ctx) {
+		String headerString = ctx.header("header-test");
+		ctx.header("header-test-response", String.format("Response: %s", headerString));
+		if (headerString != null) {
+			ctx.result(String.format("You sent %s in the header", headerString)).status(HttpStatus.OK);
 		}
+		else {
+			ctx.result("You didn't specify header-test").status(HttpStatus.BAD_REQUEST);
+		}
+	}
 }
